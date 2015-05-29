97c7559aa614d32bad2459d6ac389d9c69766ad0
Michael Brown <the-snowwhite@users.noreply.github.com>
2015-05-29 19:52:33 +0200

    Update run.py
    exporting environment for haltalk

--- configs/ARM/Beaglebone/MibRapX/run.py
+++ configs/ARM/Beaglebone/MibRapX/run.py
@@ -7,11 +7,14 @@ import importlib
 import argparse
 from time import *
 from machinekit import launcher
+from machinekit import config
 
 launcher.register_exit_handler()
 #launcher.set_debug_level(5)
 launcher.set_machinekit_ini('machinekit.ini')
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+c = config.Config()
+os.environ["MACHINEKIT_INI"] = c.MACHINEKIT_INI
 
 parser = argparse.ArgumentParser(description='This is the MibRap-X run script '
                                  'it demonstrates how a run script could look like '
