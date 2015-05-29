d04fa389aca70fae0eb06c713f5ef52e211f0e10
Michael Brown <the-snowwhite@users.noreply.github.com>
2015-05-29 20:05:40 +0200

    Update run.py

--- configs/ARM/Beaglebone/MibRapX/run.py
+++ configs/ARM/Beaglebone/MibRapX/run.py
@@ -11,7 +11,7 @@ from machinekit import config
 
 launcher.register_exit_handler()
 #launcher.set_debug_level(5)
-launcher.set_machinekit_ini('machinekit.ini')
+#launcher.set_machinekit_ini('machinekit.ini')
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 c = config.Config()
 os.environ["MACHINEKIT_INI"] = c.MACHINEKIT_INI
