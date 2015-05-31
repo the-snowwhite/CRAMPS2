fa94d080d91e83ac1d45457ec94332255b530bcf
the-snowwhite <producer@holotronic.dk>
2015-05-31 20:38:24 +0100

    Modified run.py

--- configs/ARM/Beaglebone/MibRapX/run.py
+++ configs/ARM/Beaglebone/MibRapX/run.py
@@ -7,22 +7,22 @@ import importlib
 import argparse
 from time import *
 from machinekit import launcher
-#from machinekit import config
+from machinekit import config
 
 launcher.register_exit_handler()
 #launcher.set_debug_level(5)
 #launcher.set_machinekit_ini('machinekit.ini')
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-#c = config.Config()
-#os.environ["MACHINEKIT_INI"] = c.MACHINEKIT_INI
+c = config.Config()
+os.environ["MACHINEKIT_INI"] = c.MACHINEKIT_INI
 
-parser = argparse.ArgumentParser(description='This is the MibRap-X run script '
+#parser = argparse.ArgumentParser(description='This is the MibRap-X run script '
                                  'it demonstrates how a run script could look like '
                                  'and of course starts the MibRapX config')
 
-parser.add_argument('-v', '--video', help='Starts the video server', action='store_true')
+#parser.add_argument('-v', '--video', help='Starts the video server', action='store_true')
 
-args = parser.parse_args()
+#args = parser.parse_args()
 
 try:
     launcher.check_installation()
@@ -31,8 +31,8 @@ try:
     launcher.install_comp('thermistor_check.comp')
     launcher.install_comp('reset.comp')
     launcher.start_process("configserver -n MibRap-X ~/Machineface")
-    if args.video:
-        launcher.start_process('videoserver --ini video.ini Webcam1')
+#    if args.video:
+#        launcher.start_process('videoserver --ini video.ini Webcam1')
     launcher.start_process('linuxcnc MibRapX.ini')
 #    launcher.start_process('halrun -i MibRapX.ini -I MibRapX.hal')
 except subprocess.CalledProcessError:
@@ -40,5 +40,5 @@ except subprocess.CalledProcessError:
     sys.exit(1)
 
 while True:
-  sleep(1)
-  launcher.check_processes()
+    sleep(1)
+    launcher.check_processes()
